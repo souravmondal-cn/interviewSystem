70e52f30123f0e1803a2d557b53ff2bb6b2006f5
sourav Mondal <souravm@capitalnumbers.com>
2015-12-23 19:55:36 +0530

    removed the basepath from route

--- routes.php
+++ routes.php
@@ -30,52 +30,52 @@ $app['exam.controller'] = $app->share(function () use ($app) {
     return new ExamController($app);
 });
 
-$app->get(BASEPATH."/", 'frontendHome.controller:home');
+$app->get("/", 'frontendHome.controller:home');
 
-$app->get(BASEPATH."/login", 'user.controller:getLoginFrom');
-$app->get(BASEPATH."/admin/login", 'admin.controller:getAdminLoginForm');
+$app->get("/login", 'user.controller:getLoginFrom');
+$app->get("/admin/login", 'admin.controller:getAdminLoginForm');
 
-$app->post(BASEPATH."/login", 'frontendHome.controller:doLogin');
+$app->post("/login", 'frontendHome.controller:doLogin');
 
-$app->get(BASEPATH."/register", 'user.controller:getRegistrationForm');
-$app->post(BASEPATH."/register", 'user.controller:registerUser');
+$app->get("/register", 'user.controller:getRegistrationForm');
+$app->post("/register", 'user.controller:registerUser');
 
-$app->get(BASEPATH."/logout", 'user.controller:logout');
-$app->get(BASEPATH."/dashboard", 'user.controller:dashboard');
+$app->get("/logout", 'user.controller:logout');
+$app->get("/dashboard", 'user.controller:dashboard');
 
-$app->get(BASEPATH."/admin", 'admin.controller:adminPanel');
-$app->get(BASEPATH."/adminlogout",'admin.controller:adminLogout');
+$app->get("/admin", 'admin.controller:adminPanel');
+$app->get("/adminlogout",'admin.controller:adminLogout');
 
-$app->get(BASEPATH."/questionlisting","admin.settings:questionListing");
-$app->get(BASEPATH."/questionupload","admin.settings:questionUpload");
-$app->post(BASEPATH."/questionupload","admin.settings:doUpload");
-$app->get(BASEPATH."/editQuestion/{id}", "admin.settings:editQuestion");
-$app->get(BASEPATH."/deleteQuestion/{id}", "admin.settings:deleteQuestion");
+$app->get("/questionlisting","admin.settings:questionListing");
+$app->get("/questionupload","admin.settings:questionUpload");
+$app->post("/questionupload","admin.settings:doUpload");
+$app->get("/editQuestion/{id}", "admin.settings:editQuestion");
+$app->get("/deleteQuestion/{id}", "admin.settings:deleteQuestion");
 
-$app->get(BASEPATH."/category","admin.settings:categorySetting");
-$app->get(BASEPATH."/addcategory","admin.settings:addCategory");
-$app->post(BASEPATH."/addcategory","admin.settings:doAddCategory");
-$app->get(BASEPATH."/editCategory/{id}", "admin.settings:editCategory");
-$app->get(BASEPATH."/deleteCategory/{id}", "admin.settings:deleteCategory");
+$app->get("/category","admin.settings:categorySetting");
+$app->get("/addcategory","admin.settings:addCategory");
+$app->post("/addcategory","admin.settings:doAddCategory");
+$app->get("/editCategory/{id}", "admin.settings:editCategory");
+$app->get("/deleteCategory/{id}", "admin.settings:deleteCategory");
 
-$app->get(BASEPATH."/adminsetting","admin.controller:showAdminSettings");
-$app->get(BASEPATH."/usersetting","admin.controller:showUserSettings");
-$app->get(BASEPATH."/adduser/{userType}", "admin.controller:getAddUserForm");
-$app->post(BASEPATH."/adduser/{userType}", "admin.controller:doAddUser");
-$app->get(BASEPATH."/edituser/{userType}/{id}", "admin.controller:editUserData");
-$app->get(BASEPATH."/deleteuser/{userType}/{id}", "admin.controller:deleteUserData");
+$app->get("/adminsetting","admin.controller:showAdminSettings");
+$app->get("/usersetting","admin.controller:showUserSettings");
+$app->get("/adduser/{userType}", "admin.controller:getAddUserForm");
+$app->post("/adduser/{userType}", "admin.controller:doAddUser");
+$app->get("/edituser/{userType}/{id}", "admin.controller:editUserData");
+$app->get("/deleteuser/{userType}/{id}", "admin.controller:deleteUserData");
 
-$app->post(BASEPATH."/checkEmail/{email}","admin.controller:checkUserRegistration");
-$app->get(BASEPATH."/viewfile/{filename}","admin.controller:downloadFile");
+$app->post("/checkEmail/{email}","admin.controller:checkUserRegistration");
+$app->get("/viewfile/{filename}","admin.controller:downloadFile");
 
-$app->get(BASEPATH."/examsetting","exam.settings:examSetting");
-$app->post(BASEPATH."/examsetting","exam.settings:examGenerate");
+$app->get("/examsetting","exam.settings:examSetting");
+$app->post("/examsetting","exam.settings:examGenerate");
 
-$app->get(BASEPATH."/viewHistory/{emailId}", "exam.settings:listExamHistory");
-$app->get(BASEPATH."/examdetail/{examId}", "exam.settings:viewExamDetail");
-$app->get(BASEPATH."/setQualified/{examId}", "exam.settings:setQualified");
+$app->get("/viewHistory/{emailId}", "exam.settings:listExamHistory");
+$app->get("/examdetail/{examId}", "exam.settings:viewExamDetail");
+$app->get("/setQualified/{examId}", "exam.settings:setQualified");
 
-$app->get(BASEPATH."/examstart/{userId}", "exam.controller:examStart");
-$app->get(BASEPATH."/displayQuestion", "exam.controller:displayQuestion");
-$app->post(BASEPATH."/examsubmit", "exam.controller:examSubmit");
-$app->get(BASEPATH."/examsubmit", "exam.controller:forceSubmit");
+$app->get("/examstart/{userId}", "exam.controller:examStart");
+$app->get("/displayQuestion", "exam.controller:displayQuestion");
+$app->post("/examsubmit", "exam.controller:examSubmit");
+$app->get("/examsubmit", "exam.controller:forceSubmit");
