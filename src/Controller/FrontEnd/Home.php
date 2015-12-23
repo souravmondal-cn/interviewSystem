5668d874da2e5fe20863f842215c96382948928b
sourav Mondal <souravm@capitalnumbers.com>
2015-12-23 19:16:21 +0530

    implemented the basepath settings

--- src/Controller/FrontEnd/Home.php
+++ src/Controller/FrontEnd/Home.php
@@ -33,10 +33,8 @@ class Home extends Controller {
         $loggerId = $sessionData->get('loggedInId');
 
         if (empty($loggerId)) {
-
-            $route = '/login';
+            $route = BASEPATH.'/login';
         } else {
-
             $route = $this->checkLoggerId($loggerId);
         }
 
